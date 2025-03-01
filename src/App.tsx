c69ba5106e99b039a6890b89254647c6be9246f5
Wesley Souza <souzawes93@gmail.com>
2025-03-01 20:18:55 -0300

    feat: Call components in main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import "./global.css";
+import "./styles/App.css";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="app-container">
+      <ProductList />
+      <Cart />
+    </div>
+  );
 }
 
-export default App
+export default App;
